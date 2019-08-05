7107c4ca86e7e43bb71fe9a7f37a3dc85aa57f4b
Swaraj <swarajchavan540@gmail.com>
2019-08-05 18:23:18 +0530

    embed javascript for season

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
+// var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set("view engine", "ejs");
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + "/public"));
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+    
+
+//====ROUTES=====//
+app.get('/', function(req, res){
     res.render('home');
 });
-
 app.listen(3000, ()=>{
     console.log('Server started!');
 });
