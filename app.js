33a427070ec5121de9e711859cf7cc59bb911315
Swaraj <swarajchavan540@gmail.com>
2019-09-23 21:20:28 +0530

    restart

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true   }));
 
 
 //====ROUTES=====//
