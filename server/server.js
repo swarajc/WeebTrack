b5ba2554efc9c2bf3917b0ba00ebe73ac0c3c583
Swaraj <swarajchavan540@gmail.com>
2019-09-24 06:13:31 +0530

    add cors, logger, mongoose  to backE

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
-var express    = require('express'),
-    request    = require('request'),
-    Jikan      = require('jikan-node'),
-    bodyParser = require('body-parser');
+const   express    = require('express'),
+        request    = require('request'),
+        Jikan      = require('jikan-node'),
+        bodyParser = require('body-parser'),
+        logger     = require('morgan'),
+        Data       = require('./data');
+
+var cors = require('cors');
 
 var app = express();
+
 const mal = new Jikan();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true   }));
-
+app.use(bodyParser.json());
+app.use(logger('dev'));
 
 //====ROUTES=====//
 
