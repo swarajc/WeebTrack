652cffa029c04125ee98e9bdc0cbca180a0aa650
Swaraj <swarajchavan540@gmail.com>
2019-09-29 23:38:52 +0530

    fix

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function Home(){
+    return(
+        <div id = 'homeDiv'>
+            <h1 className = 'homeHeader'>Welcome to WeebTrack!</h1>
+              
+        </div>       
+    );
+}
+
+export default Home;
