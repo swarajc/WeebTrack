127bc732b94cfd23c327fa8f8c5bf87360a0b7d3
Swaraj <swarajchavan540@gmail.com>
2019-10-02 16:23:19 +0530

    update form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,65 +1,82 @@
-import React from 'react';
-import '../App.css';    
+import React, {Component} from 'react';
+import '../App.css';
+import VisibilityIcon from '@material-ui/icons/Visibility';  
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';  
 
-function Home(){
-    return(
-        <div className="container">
-            <div className="homeHeader">
-                <h1>Welcome to WeebTrack!</h1>
-                <p>Now track your anime and manga with ease.</p>
-            </div>
-            <div>
-                <form action = "" method = "POST">
-                    
-                    <div className = "card">
-                        <div className = "card-content">
 
-                            <div className = "form-item-divs">
-                                <label htmlFor = "username">
-                                    Username
-                                </label>
-                                <div>
-                                    <input id = "username" type= "text"/>    
-                                </div>            
-                            </div>
-                            
-                            <div className = "form-item-divs">
-                                <label htmlFor = "emailid">
-                                    Email address
-                                </label>
-                                <div>
-                                    <input id = "emailid" type= "email"/>
+class Home extends Component{
+
+    handleClick = (e) => {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      }
+      
+    render(){
+        return(
+
+            <div className="container">
+                <div className="homeHeader">
+                    <h1>Welcome to WeebTrack!</h1>
+                    <p>Now track your anime and manga with ease.</p>
+                </div>
+                <div>
+                    <form action = "" method = "POST">
+                        
+                        <div className = "card">
+                            <div className = "card-content">
+                                <h2>Create an account</h2>
+    
+                                <div className = "form-item-divs">
+                                    <label htmlFor = "username">
+                                        Username
+                                    </label>
+                                    <div>
+                                        <input id = "username" type= "text" required/>    
+                                    </div>            
+                                </div>
+                                
+                                <div className = "form-item-divs">
+                                    <label htmlFor = "emailid">
+                                        Email address
+                                    </label>
+                                    <div>
+                                        <input id = "emailid" type= "email" required/>
+                                    </div>
+                                </div>
+                                
+                                <div className = "form-item-divs">
+                                    <label htmlFor = "password">
+                                        Password
+                                    </label>
+                                    <div className = "input-icon-wrap">   
+                                        <span className = "input-icon" type="button" onClick = {this.handleClick}><VisibilityIcon className= "Icon"></VisibilityIcon></span>
+                                        <input className="input-with-icon" id = "password" type= "password" required/>
+                                    </div>
+                                </div>
+                                
+                                <div className = "form-item-divs">
+                                    <label htmlFor = "cpassword">
+                                        Confirm password
+                                    </label>
+                                    <div>
+                                        <input id = "cpassword" type= "password" required/>
+                                    </div>
+                                </div>
+                                <div className = "buttonDiv form-item-divs">
+                                        <button>Sign Up</button>
                                 </div>
                             </div>
-                            
-                            <div className = "form-item-divs">
-                                <label htmlFor = "password">
-                                    Password
-                                </label>
-                                <div>
-                                    <input id = "password" type= "password"/>
-                                </div>
-                            </div>
-                            
-                            <div className = "form-item-divs">
-                                <label htmlFor = "cpassword">
-                                    Confirm password
-                                </label>
-                                <div>
-                                    <input id = "cpassword" type= "password"/>
-                                </div>
-                            </div>
-                            <div className = "buttonDiv form-item-divs">
-                                    <button>Sign Up</button>
-                            </div>
-                            
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-               
-    );
+        );
+    }    
+    
 }
 
 export default Home;
