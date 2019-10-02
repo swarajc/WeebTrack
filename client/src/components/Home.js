79a92e556daeec5be12cb6b0a2b9866087bf0d57
Swaraj <swarajchavan540@gmail.com>
2019-10-02 18:30:22 +0530

    update form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../App.css';
-import VisibilityIcon from '@material-ui/icons/Visibility';  
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';  
+// import { makeStyles } from '@material-ui/core/styles';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+// import { IconButton } from '@material-ui/core';    
+  
+
+// const useStyles = makeStyles({
+//     EyeIconButton:{
+//         padding: "10%",
+//     },
 
 
-class Home extends Component{
+// });
 
-    handleClick = (e) => {
+export default function Home(){
+
+    // const classes = useStyles();
+    const handleClick = (e) => {
         var x = document.getElementById("password");
         if (x.type === "password") {
           x.type = "text";
@@ -14,69 +24,64 @@ class Home extends Component{
           x.type = "password";
         }
       }
-      
-    render(){
-        return(
-
-            <div className="container">
-                <div className="homeHeader">
-                    <h1>Welcome to WeebTrack!</h1>
-                    <p>Now track your anime and manga with ease.</p>
-                </div>
-                <div>
-                    <form action = "" method = "POST">
-                        
-                        <div className = "card">
-                            <div className = "card-content">
-                                <h2>Create an account</h2>
     
-                                <div className = "form-item-divs">
-                                    <label htmlFor = "username">
-                                        Username
-                                    </label>
-                                    <div>
-                                        <input id = "username" type= "text" required/>    
-                                    </div>            
-                                </div>
-                                
-                                <div className = "form-item-divs">
-                                    <label htmlFor = "emailid">
-                                        Email address
-                                    </label>
-                                    <div>
-                                        <input id = "emailid" type= "email" required/>
-                                    </div>
-                                </div>
-                                
-                                <div className = "form-item-divs">
-                                    <label htmlFor = "password">
-                                        Password
-                                    </label>
-                                    <div className = "input-icon-wrap">   
-                                        <span className = "input-icon" type="button" onClick = {this.handleClick}><VisibilityIcon className= "Icon"></VisibilityIcon></span>
-                                        <input className="input-with-icon" id = "password" type= "password" required/>
-                                    </div>
-                                </div>
-                                
-                                <div className = "form-item-divs">
-                                    <label htmlFor = "cpassword">
-                                        Confirm password
-                                    </label>
-                                    <div>
-                                        <input id = "cpassword" type= "password" required/>
-                                    </div>
-                                </div>
-                                <div className = "buttonDiv form-item-divs">
-                                        <button>Sign Up</button>
+    return(
+        <div className="container">
+            <div className="homeHeader">
+                <h1>Welcome to WeebTrack!</h1>
+                <p>Now track your anime and manga with ease.</p>
+            </div>
+            <div>
+                <form action = "" method = "POST">
+                    
+                    <div className = "card">
+                        <div className = "card-content">
+                            <h2>Create an account</h2>
+
+                            <div className = "form-item-divs">
+                                <label htmlFor = "username">
+                                    Username
+                                </label>
+                                <div>
+                                    <input id = "username" type= "text" required/>    
+                                </div>            
+                            </div>
+                            
+                            <div className = "form-item-divs">
+                                <label htmlFor = "emailid">
+                                    Email address
+                                </label>
+                                <div>
+                                    <input id = "emailid" type= "email" required/>
                                 </div>
                             </div>
+                            
+                            <div className = "form-item-divs">
+                                <label htmlFor = "password">
+                                    Password
+                                </label>
+                                <div className = "input-icon-wrap">   
+                                    <span className = "input-icon" type="button" onClick = {handleClick}><VisibilityIcon className= "Icon"></VisibilityIcon></span>
+                                    {/* <span className = "input-icon"><IconButton className = {classes.EyeIconButton} onClick = {handleClick}><VisibilityIcon className= "Icon"/></IconButton></span> */}
+                                    <input className="input-with-icon" id = "password" type= "password" required/>
+                                </div>
+                            </div>
+                            
+                            <div className = "form-item-divs">
+                                <label htmlFor = "cpassword">
+                                    Confirm password
+                                </label>
+                                <div>
+                                    <input id = "cpassword" type= "password" required/>
+                                </div>
+                            </div>
+                            <div className = "buttonDiv form-item-divs">
+                                <button>Sign Up</button>
+                            </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
-        );
-    }    
-    
+        </div>
+    );
 }
-
-export default Home;
