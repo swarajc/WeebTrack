1cc844caf5cd6d3e885ffe92ce4a53d27774b9a4
Swaraj <swarajchavan540@gmail.com>
2019-10-23 23:20:47 +0530

    add Season and Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Home from './components/Home';   
+import Home from './components/Home'
+import SignUp from './components/SignUp';   
 
 function App(){
     return(
-        <Home />
-    )       
+        <React.Fragment>
+            <Home />
+            <SignUp />
+        </React.Fragment>
+    )
 }
 
 export default App;
---
+++ client/src/components/Season.js
@@ -0,0 +1,22 @@
+var currentDate = new Date(),
+    month = currentDate.getMonth();  
+
+    var season = '';
+    if( month >= 3 && month <= 5)
+    {
+        season = 'Spring';
+    }
+    else if(month >= 6 && month <= 8 )
+    {
+        season  = 'Summer';
+    }
+    else if(month >= 9 && month <= 11)
+    {
+        season = 'Fall';
+    }
+    else
+    {
+        season = 'Winter';
+    }
+
+    export default season;
