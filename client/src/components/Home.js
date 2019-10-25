a61a69d4b9a8e68022931c4e99ec6fe5d9d59d5b
Swaraj <swarajchavan540@gmail.com>
2019-10-25 11:25:23 +0530

    add jikan api

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import season from './Season.js'
 import '../styles/Home.css'
+import {useState, useEffect} from 'react'
 
 const Home = () => {
-        return(
-            <div>
-                <h1>Trending Anime of the {season}</h1>
+    var page = 1;
+    useEffect(() => {
+        fetch(`https://api.jikan.moe/v3/top/anime/${page}/airing`)
+            .then(res => res.json)
+            .catch(error => console.log(error));
 
-            </div>   
+    }, [page]
         )
-    }
+    return(
+        <div>
+            <h1>Trending Anime of the {season}</h1>         
+            <div className = 'animeCovers'>
+                 
+            </div>
+        </div> 
+    )    
+}
 
 export default Home ;
