ff860defb37c8ad4d6b2113653598d8afec6f792
Swaraj <swarajchavan540@gmail.com>
2019-12-17 21:12:40 +0530

    added forgot pass css file

--- client/src/components/forgotpass.js
+++ client/src/components/forgotpass.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import '../styles/forgotpass.css';
 const forgotpass = () => {
 
     return (
-        <div className = 'container'>
+        <div className = 'fcontainer'>
             <form action="" method = "POST">
                 <div className="card">
                     <div className="card-content">
                         {/* <h2>Welcome to WeebTrack!</h2>
                             <p>Now track yo ur anime and manga with ease.</p> */}
                         <h1>Forgot Password</h1>
-                        <div className="form-item-divs">
+                        <div>
                             <label htmlFor="emailid">
                                 Email address
                                 </label>
