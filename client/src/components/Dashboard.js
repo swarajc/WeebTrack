44f7ce88ceb399de2cab684cf0dfe43b8a627889
Swaraj <swarajchavan540@gmail.com>
2020-01-21 23:55:43 +0530

    add buttons to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Dashboard.css';
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 
@@ -9,7 +9,7 @@ export default function Dashboard({ history, caughtToken, parentCallBack }) {
     const isMounted = useRef(true);
     console.log(caughtToken);
     const token = caughtToken;
-
+    var [UserName, setUserName] = useState('User');
     useEffect(() => {
 
         if (isMounted.current === true) {
@@ -38,7 +38,8 @@ export default function Dashboard({ history, caughtToken, parentCallBack }) {
                         if (info) {
 
                             console.log(info);
-
+                            setUserName(String(info.username));
+                            console.log(UserName);
                         }
                         else
                             if (info.error) {
@@ -56,7 +57,7 @@ export default function Dashboard({ history, caughtToken, parentCallBack }) {
         window.onpopstate = (e) => {
             history.push('/u/dashboard');
         }
-    })
+    }, [UserName])
 
     const handleSubmit = (e) => {
 
@@ -83,6 +84,7 @@ export default function Dashboard({ history, caughtToken, parentCallBack }) {
 
                     console.log(info);
                     caughtToken = '';
+
                     parentCallBack(caughtToken);
                     history.push('/home');
                 }
@@ -97,14 +99,25 @@ export default function Dashboard({ history, caughtToken, parentCallBack }) {
             .catch((error) => console.log(error));
 
     }
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div className='logOut'>
-                    <button className = 'b-logOut' type='submit'>Sign out</button>
-                </div>
+            <div className='d-buttons'>
+               
+                <form onSubmit={handleSubmit}>
 
-            </form>
+                    <div className='logOut'>
+                        <button className='b-logOut' type='submit'>Sign out</button>
+                    </div>
+
+                </form>
+                <form action="">
+                    <div className='profile'>
+                        <button className='b-profile'>Welcome {UserName}</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 }
