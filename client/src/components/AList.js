41993f89f7d7235cef12503d6bb1802d76365f18
Swaraj <swarajchavan540@gmail.com>
2020-05-11 15:28:04 +0530

    add basic list component

--- client/src/components/AList.js
+++ client/src/components/AList.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import '../styles/Dashboard.css';
 import { useState, useEffect, useRef } from 'react';
 import '../styles/AList.css'
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: 752,
+    },
+    demo: {
+        backgroundColor: theme.palette.background.transparent
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
+    },
+}));
 
 export default function AList({ history, caughtToken }) {
+
+
 
     const isMounted = useRef(true);
 
     const token = caughtToken;
 
     const [animes, setAnimes] = useState([]);
+
+    const classes = useStyles();
 
     useEffect(() => {
 
@@ -60,14 +86,35 @@ export default function AList({ history, caughtToken }) {
 
     return animes ? (
         <div className='lcontainer'>
-            <h1>Anime List</h1>
-            {
-                animes.map(anime => (
-                    <div key={anime.anime}>
-                        <p>{anime.anime}</p>
+            <div className={classes.root}>
+                <Grid>
+                    <h1>Anime List</h1>
+                    <div className={`${classes.demo} a-list`}>
+                        <List>
+                            {
+                                animes.map(anime => (
+                                    <ListItem key={anime.anime}>
+                                        <ListItemAvatar>
+
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary={anime.anime}
+                                        />
+                                        <ListItemSecondaryAction>
+                                            <IconButton edge="end" aria-label="delete">
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>
+                                ))
+                            }
+
+                        </List>
                     </div>
-                ))
-            }
+                </Grid>
+            </div>
+
+
         </div>
     ) : (<p>Loading...</p>)
 }
